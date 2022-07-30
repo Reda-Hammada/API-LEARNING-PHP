6a0ca8dc0626e76d3fb4e34a97ba182fb5ff7a1f
Reda Hammada <hammada.reda.dev@gmail.com>
2022-07-30 18:13:41 +0200

    consume an api with cURL

--- curl/curl.php
+++ curl/curl.php
@@ -1,4 +1,19 @@
 <?php
 
+$curl_session = curl_init();
 
-$curl = curl_init();
+curl_setopt($curl_session,CURLOPT_URL,"https://randomuser.me/api");
+curl_setopt($curl_session,CURLOPT_RETURNTRANSFER,true );
+curl_setopt($curl_session,CURLOPT_HEADER, false);
+
+$result = curl_exec($curl_session);
+
+curl_close($curl_session);
+
+echo $result;
+
+
+
+
+?>
+
